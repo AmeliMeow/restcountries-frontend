6bad6fee47a3994ae354f69a0e2bf217b5e92d1c
AmeliMeow <amelia_mae@tuta.io>
2023-01-31 21:11:44 +0200

    Added button and made list sortable by name

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import 'normalize.css'
 import './App.css';
 import { useEffect, useState } from 'react';
+import { IoChevronDownOutline, IoChevronUpOutline } from "react-icons/io5";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [sort_order, setSortOrder] = useState('asc');
 
   useEffect(() => {
     fetch('https://restcountries.com/v2/all?fields=name,region,area')
     .then(resp => resp.json())
     .then(data => setCountries(data))
-    .catch(reason => console.log(reason))
+    .catch(reason => console.log(reason));
   }, []);
+
+  const btn_sort_click = () => {
+    const s_order = sort_order === 'desc' ? 'asc' : 'desc';
+    setSortOrder(s_order);
+    setCountries(prevCountries => 
+      prevCountries.sort((a, b) => {
+        if (s_order === 'asc') {
+          return a.name.localeCompare(b.name)
+        }
+        return b.name.localeCompare(a.name)
+      })
+    );
+  }
 
   const ListItem = (props) => {
     const {name, region, area, independent} = props.data;
@@ -19,7 +34,7 @@ function App() {
         <h2>{name}</h2>
         <p><span className='bold'>Region: </span>{region}</p>
         <p><span className='bold'>Area: </span>{area} km<sup>2</sup></p>
-        <p><span className='bold'>Independant: </span>{independent ? "Yes" : "No"}</p>
+        {/* <p><span className='bold'>Independant: </span>{independent ? "Yes" : "No"}</p> */}
       </div>
     );
   };
@@ -28,6 +43,9 @@ function App() {
     <div className="app">
       <div className='top-bar'>
         <h1>Restcountries API Front-End</h1>
+        <div className='row'>
+          <button className='btn' onClick={btn_sort_click}>Sort by name {sort_order === 'desc' ? <IoChevronDownOutline /> : <IoChevronUpOutline />}</button>
+        </div>
       </div>
       {countries.length > 0 &&
         <div className='country-list'>
