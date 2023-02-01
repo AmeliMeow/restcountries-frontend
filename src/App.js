055509d192f8873c60c2bf366d625b4c3e3530d5
AmeliMeow <amelia_mae@tuta.io>
2023-02-01 17:25:12 +0200

    Added filters

--- src/App.js
+++ src/App.js
@@ -1,56 +1,106 @@
-import 'normalize.css'
-import './App.css';
-import { useEffect, useState } from 'react';
+import "normalize.css";
+import "./App.css";
+import { useEffect, useState } from "react";
 import { IoChevronDownOutline, IoChevronUpOutline } from "react-icons/io5";
+
+let lt_area;
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [sort_order, setSortOrder] = useState('asc');
+  const [data_output, setOutput] = useState([]);
+  const [sort_order, setSortOrder] = useState("asc");
 
   useEffect(() => {
-    fetch('https://restcountries.com/v2/all?fields=name,region,area')
-    .then(resp => resp.json())
-    .then(data => setCountries(data))
-    .catch(reason => console.log(reason));
+    fetch("https://restcountries.com/v2/all?fields=name,region,area")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCountries(data);
+        setOutput(data);
+        lt_area = data.find((el) => el.name === "Lithuania").area; // Get area of Lithuania
+      })
+      .catch((reason) => console.log(reason));
   }, []);
 
   const btn_sort_click = () => {
-    const s_order = sort_order === 'desc' ? 'asc' : 'desc';
+    const s_order = sort_order === "desc" ? "asc" : "desc";
     setSortOrder(s_order);
-    setCountries(prevCountries => 
-      prevCountries.sort((a, b) => {
-        if (s_order === 'asc') {
-          return a.name.localeCompare(b.name)
+    setOutput((prevOutput) =>
+      prevOutput.sort((a, b) => {
+        if (s_order === "asc") {
+          return a.name.localeCompare(b.name);
         }
-        return b.name.localeCompare(a.name)
+        return b.name.localeCompare(a.name);
       })
     );
-  }
+  };
+
+  const filterData = (filter) => {
+    if ("less_than_lt_area" === filter) {
+      setOutput(countries.filter((country) => country.area < lt_area));
+    } else if ("oceania" === filter) {
+      setOutput(countries.filter((country) => country.region === "Oceania"));
+    } else if ("both" === filter) {
+      setOutput(
+        countries.filter(
+          (country) => country.region === "Oceania" && country.area < lt_area
+        )
+      );
+    } else {
+      setOutput(countries);
+    }
+  };
 
   const ListItem = (props) => {
-    const {name, region, area} = props.data;
+    const { name, region, area } = props.data;
     return (
-      <div className='list-item'>
+      <div className="list-item">
         <h2>{name}</h2>
-        <p><span className='bold'>Region: </span>{region}</p>
-        <p><span className='bold'>Area: </span>{area} km<sup>2</sup></p>
+        <p>
+          <span className="bold">Region: </span>
+          {region}
+        </p>
+        <p>
+          <span className="bold">Area: </span>
+          {area} km<sup>2</sup>
+        </p>
       </div>
     );
   };
 
   return (
     <div className="app">
-      <div className='top-bar'>
+      <div className="top-bar">
         <h1>Restcountries API Front-End</h1>
-        <div className='row'>
-          <button className='btn' onClick={btn_sort_click}>Sort by name {sort_order === 'desc' ? <IoChevronDownOutline /> : <IoChevronUpOutline />}</button>
+        <div className="row">
+          <button className="btn" onClick={btn_sort_click}>
+            Sort by name{" "}
+            {sort_order === "desc" ? (
+              <IoChevronDownOutline />
+            ) : (
+              <IoChevronUpOutline />
+            )}
+          </button>
+          <div className="select">
+            <select onChange={(event) => filterData(event.target.value)}>
+              <option>Filter by...</option>
+              <option value="less_than_lt_area">
+                Countries smaller than Lithuania by area
+              </option>
+              <option value="oceania">Countries in Oceania region</option>
+              <option value="both">
+                Countries smaller than Lithuania by area in Oceania
+              </option>
+            </select>
+          </div>
         </div>
       </div>
-      {countries.length > 0 &&
-        <div className='country-list'>
-          {countries.map((country, i) => <ListItem key={i} data={country}></ListItem>)}
+      {countries.length > 0 && (
+        <div className="country-list">
+          {data_output.map((country, i) => (
+            <ListItem key={i} data={country}></ListItem>
+          ))}
         </div>
-      }
+      )}
     </div>
   );
 }
